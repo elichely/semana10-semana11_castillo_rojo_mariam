d09eb16b40488081e9af3f8c8d69a19e798490cf
elichely <71729823+elichely@users.noreply.github.com>
2021-06-28 09:22:22 -0600

    segunda sesion correspondiente a 14/05/21 se agrego activity age se agrego phone activity se agrego activity contact 1 y 2

---
+++ app/src/main/java/com/example/proyecto10_semana_11/Contact.kt
@@ -0,0 +1,10 @@
+package com.example.proyecto10_semana_11
+
+class Contact {
+    class Contact {
+        var Nombre: String? = null
+        var Apellidos: String? = null
+        var Telefono: String? = null
+        var CorreoElectronico: String? = null
+}
+}
---
+++ app/src/main/java/com/example/proyecto10_semana_11/ContactActivity1.kt
@@ -0,0 +1,16 @@
+package com.example.proyecto10_semana_11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class ContactActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact1)
+    }
+
+    fun sendData(view: View) {
+
+    }
+}
